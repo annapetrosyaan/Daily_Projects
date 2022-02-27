2b47e9965c5b838c59d41413633bac9a5abfb0b4
annapetrosyaan <89910608+annapetrosyaan@users.noreply.github.com>
2022-02-28 00:41:38 +0400

    Rename Person.cpp to CppCode.cpp

--- Person_Class/CppCode.cpp
+++ Person_Class/CppCode.cpp
@@ -4,14 +4,14 @@ private:
 	std::string name;
 	int age;
 public:
-	Person() {
-		age=0;	}
+	Person() { age=0;}
 	Person(std::string s,int a): name(s),age(a){}
 	int getage() { return age; }
 	std::string getname() { return name; }
 	void setage(int a) { age = a; }
 	void setname(std::string str) { name = str; }
-	Person(const Person& obj) {
+	Person(const Person& obj)
+	{
 		age = obj.age;
 		name = obj.name;
 	}
